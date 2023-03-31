4b10061667c4e697a230d19fd11fe0cf066422bf
ogawayouji <satsumaimo697@gmail.com>
2023-03-31 13:34:00 +0900

    feat: 一覧ページ作成

---
+++ src/pages/components/Articles.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../styles/Articles.css'
+
+export const Articles = () => {
+  return (
+    <div className="articles">
+      <p className="title">All articles</p>
+    </div>
+  )
+}
